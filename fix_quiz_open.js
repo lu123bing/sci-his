cd63b68974c576a6f349c0aad50bf6f86a4fbbac
karinen <12312702@mail.sustech.edu.cn>
2026-05-25 22:27:43 +0800

    add some visual emphasis

--- fix_quiz_open.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let build = fs.readFileSync('build.js', 'utf8');
-
-build = build.replace(
-  /const defaultOpen = isQuiz \? '' : 'open';/,
-  `const defaultOpen = 'open';`
-);
-
-fs.writeFileSync('build.js', build);
